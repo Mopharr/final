6fe95977589ef6677f6018e6e285773f465def54
quadri salawudeen <olamiquadri1@gmail.com>
2021-06-17 02:19:05 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#hamburger')
+
+const menuLinks = document.querySelector('.dashboard')
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
