4946209d76170e0d63cd43327d080451447e312e
Bashsoft707 <alausabashir@gmail.com>
2021-06-30 02:21:15 -1200

    commit

--- js/reg.js
+++ js/reg.js
@@ -1,11 +1,47 @@
-var k = "The respective values are :";
+var k = "";
     function Login() {
         var input = document.getElementsByName('array[]');
+        var error = document.getElementById('error');
   
         for (var i = 0; i < input.length; i++) {
             var a = input[i];
             k = k + a.value + " ";
         }
-  
-        document.getElementById("par").innerHTML = k;
+        console.log(k);
+
+        const url = 'https://unique-id-final-year-project.herokuapp.com/'
+        const data = {
+            input: k,
+        }
+        const fetchData = {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+
+            body:  JSON.stringify(data),
+        
+        }
+
+        console.log(data)
+        fetch(url, fetchData)
+        .then(resp => {
+            if(resp.ok) {
+                return resp.json()
+            } else {
+                return Promise.reject("Oops!!! Something went wrong.")
+            }
+        })
+        .then(data => {
+            console.log(data)
+            if (data.Error === 0) {
+                input = "";
+                localStorage.setItem('userToken', data.token)
+                self.location = '/'
+                console.log("succesful");
+            } else {
+                error.innerHTML = data.Message
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
