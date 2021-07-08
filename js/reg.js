bacaad71b843423f7d069359994b13eda71561a5
quadri salawudeen <olamiquadri1@gmail.com>
2021-07-08 12:34:16 +0100

    fix

--- js/reg.js
+++ js/reg.js
@@ -3,19 +3,54 @@ var k = "";
         var input = document.getElementsByName('array[]');
         var error = document.getElementById('error');
   
-        for (var i = 0; i < input.length; i++) {
-            var a = input[i];
-            k = k + a.value + " ";
-        }
-        console.log(k);
+        var childname = document.querySelector('#childname').value
+        var DOB = document.querySelector('#dob').value
+        var sex = document.querySelector('#sex').value
+        var place = document.querySelector('#poc').value
+        var village = document.querySelector('#village').value
+        var localGovt = document.querySelector('#local').value
+        var state = document.querySelector('#sob').value
+        var mName = document.querySelector('#motherName').value
+        var maddress = document.querySelector('#address').value
+        var maritalStatus = document.querySelector('#status').value
+        var mnation = document.querySelector('#nationality').value
+        var mstate = document.querySelector('#motherstate').value
+        var moccupation = document.querySelector('#occupation').value
+        var fName = document.querySelector('#fatherName').value
+        var faddress = document.querySelector('#fatherAddress').value
+        var fnationality = document.querySelector('#fnationality').value
+        var fstate = document.querySelector('#fstate').value
+        var foccupation = document.querySelector('#foccupation').value
+        var token = localStorage.getItem('userToken')
 
-        const url = 'https://unique-id-final-year-project.herokuapp.com/'
+
+        const url = 'https://unique-id-final-year-project.herokuapp.com/user_reg'
         const data = {
-            input: k,
+            nameOfChild: childname,
+            dob: DOB,
+            sex: sex,
+            placeOfBirth: place,
+            villageOfBirth: village,
+            lgaOfBirth: localGovt,
+            stateOfBirth: state,
+            motherName: mName,
+            motherAddress: maddress,
+            motherMaritalStatus: maritalStatus,
+            motherNationality: mnation,
+            motherStateOfOrigin: mstate,
+            motherOccupation: moccupation,
+            fatherName: fName,
+            fatherAddress: faddress,
+            fatherNationality: fnationality,
+            fatherStateOfOrigin:fstate,
+            fatherOccupation: foccupation,
+
         }
         const fetchData = {
             method: 'post',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
 
             body:  JSON.stringify(data),
         
@@ -33,7 +68,25 @@ var k = "";
         .then(data => {
             console.log(data)
             if (data.Error === 0) {
-                input = "";
+                childname = ''
+                DOB = ''
+                sex = ''
+                place = ''
+                village = ''
+                localGovt = ''
+                state = ''
+                mName = ''
+                maddress = ''
+                maritalStatus = ''
+                mnation = ''
+                mstate = ''
+                moccupation =''
+                fName = ''
+                faddress = ''
+                fnationality = ''
+                fstate = ''
+                foccupation = ''
+
                 localStorage.setItem('userToken', data.token)
                 self.location = '/'
                 console.log("succesful");
