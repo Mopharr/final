27fe4116956550416174dc68324ac283b5cb4bc4
quadri salawudeen <olamiquadri1@gmail.com>
2021-07-17 14:37:57 +0100

    commit

--- js/preview.js
+++ js/preview.js
@@ -1,13 +1,19 @@
 
  const cname = document.querySelector('#cname')
  const DOB = document.querySelector('#DOB')
-//  const regId = document.querySelector('#regId')
+ const regId = document.querySelector('#regId')
  const localGovt = document.querySelector('#localGovt')
  const state = document.querySelector('#state')
  const sex = document.querySelector('#sex')
+ const placeOfOccurence = document.querySelector('#POC')
  const fName = document.querySelector('#fName')
+ const fAddress = document.querySelector('#fAddress')
+ const fState = document.querySelector('#fState')
  const fOccupation = document.querySelector('#fOccupation')
  const mName = document.querySelector('#mName')
+ const mAddress = document.querySelector('#mAddress')
+ const mStatus = document.querySelector('#mStatus')
+ const mState = document.querySelector('#mState')
  const mOccupation = document.querySelector('#mOccupation')
  
 
@@ -20,13 +26,19 @@ if (userRecord === null) {
     console.log(userRecord)
     cname.innerHTML = userRecord.nameOfChild
     DOB.innerHTML = userRecord.dob
-    // regId.innerHTML = userRecord.uniqueUserId
+    regId.innerHTML = userRecord.uniqueUserId
     localGovt.innerHTML = userRecord.lgaOfBirth
     state.innerHTML = userRecord.stateOfBirth
     sex.innerHTML = userRecord.sex
+    placeOfOccurence.innerHTML = userRecord.placeOfBirth
     fName.innerHTML = userRecord.fatherName
+    fAddress.innerHTML = userRecord.fatherAddress
+    fState.innerHTML = userRecord.fatherStateOfOrigin
     fOccupation.innerHTML = userRecord.fatherOccupation
     mName.innerHTML = userRecord.motherName
+    mAddress.innerHTML = userRecord.motherAddress
+    mStatus.innerHTML = userRecord.motherMaritalStatus
+    mState.innerHTML = userRecord.motherStateOfOrigin
     mOccupation.innerHTML = userRecord.motherOccupation
     
 
